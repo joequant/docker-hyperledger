f189103d744686bd3c1d3ef33b41fbcad74a04c3
Joseph C Wang <joequant@gmail.com>
2016-04-18 08:08:08 +0800

    add local deploy

--- fabric-scripts/deploy.sh
+++ fabric-scripts/deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 export CORE_PEER_ADDRESS=172.17.0.2:30303
+
 fabric chaincode deploy -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'
+
+if [ ! -d /opt/gopath/src/github.com/joequant/hyperledger ] ; then
+    mkdir -p /opt/gopath/src/github.com/joequant
+    pushd /opt/gopath/src/github.com/joequant > /dev/null
+    git clone http://github.com/joequant/hyperledger.git
+    popd > /dev/null
+fi
+
+pushd /opt/gopath/src/github.com/joequant/hyperledger
+git pull
+popd
+
+fabric chaincode deploy -p github.com/joequant/hyperledger/examples/chaincode/go/map -c '{"Function":"init", "Args": []}'

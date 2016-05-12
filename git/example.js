86da2afec117a6117d847141551ff5427dbebb7f
Joseph C Wang <joequant@gmail.com>
2016-05-12 23:45:24 +0800

    checkpoint example.js

---
+++ git/example.js
@@ -0,0 +1,52 @@
+var logger = {log: console.log, error: console.error, debug: console.log, warn: console.log};
+var Ibc1 = require('hyperledger-fabric-js');
+	var ibc = new Ibc1(/*logger*/);             //you can pass a logger such as winston here - optional
+	var chaincode = {};
+
+	// ==================================
+	// configure ibc-js sdk
+	// ==================================
+	var options = 	{
+		network:{
+			peers:   [{
+				"api_host": "172.17.0.2",
+				"api_port": "5000",
+			    "id": "vp1",
+
+			}],
+		    options: {							//this is optional
+			tls: false,
+				quiet: true, 
+				timeout: 60000
+			}
+		},
+		chaincode:{
+			zip_url: 'https://github.com/ibm-blockchain/marbles-chaincode/archive/master.zip',
+			unzip_dir: 'marbles-chaincode-master/part2',
+			git_url: 'https://github.com/ibm-blockchain/marbles-chaincode/part2'
+		}
+	};
+	
+	// Step 2 ==================================
+	ibc.load(options, cb_ready);
+
+	// Step 3 ==================================
+	function cb_ready(err, cc){								//response has chaincode functions
+	    chaincode = cc;
+	    logger.log('deployed_name ', cc);
+	// Step 4 ==================================
+	    if(!cc.details.deployed_name || cc.details.deployed_name === ""){				//decide if I need to deploy or not
+		cc.deploy('init', ['99'], null, null, cb_deployed);
+		}
+		else{
+			logger.log('chaincode summary file indicates chaincode has been previously deployed');
+			cb_deployed();
+		}
+	}
+
+	// Step 5 ==================================
+function cb_deployed(err){
+    logger.log(err);
+		console.log('sdk has deployed code and waited');
+		chaincode.query.read(['a']);
+	}

96de08a3badbb13c02464d0bd7d85f8d68dc0096
Joseph C Wang <joequant@gmail.com>
2016-05-23 18:31:11 +0800

    deploy only local

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,4 @@ export CORE_PEER_ADDRESS=172.17.0.2:30303
 
 peer chaincode deploy -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'
 
-if [ ! -d /opt/gopath/src/github.com/joequant/hyperledger ] ; then
-    mkdir -p /opt/gopath/src/github.com/joequant
-    pushd /opt/gopath/src/github.com/joequant > /dev/null
-    git clone http://github.com/joequant/hyperledger.git
-    popd > /dev/null
-fi
-
-pushd /opt/gopath/src/github.com/joequant/hyperledger
-git pull
-popd
-
 #peer chaincode deploy -p github.com/joequant/hyperledger/examples/chaincode/go/map -c '{"Function":"init", "Args": []}'

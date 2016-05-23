d69e9c29ed5787928d555ec9ccbb45f927d81293
Joseph C Wang <joequant@gmail.com>
2016-05-23 09:43:06 +0800

    add ca

--- startnet.sh
+++ startnet.sh
@@ -2,8 +2,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker stop vp0
 docker stop vp1
+docker stop ca
 docker rm vp0
 docker rm vp1
+docker rm ca
 
 docker run joequant/hyperledger echo
 
@@ -24,3 +26,11 @@ docker run --name=vp1 \
 CORE_PEER_ID=vp1 -e CORE_PEER_ADDRESSAUTODETECT=true -e \
 CORE_PEER_DISCOVERY_ROOTNODE=172.17.0.2:30303 joequant/hyperledger \
 peer node start --logging-level=debug >& vp1.log &
+
+docker run --name=ca \
+       --restart=unless-stopped \
+       -v $SCRIPT_DIR:/local-dev \
+       -i -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 -e \
+CORE_PEER_ID=ca -e CORE_PEER_ADDRESSAUTODETECT=true -e \
+       CORE_PEER_DISCOVERY_ROOTNODE=172.17.0.2:30303 joequant/hyperledger \
+       ./membersrvc --logging-level=debug >& ca.log &
--- stopnet.sh
+++ stopnet.sh
@@ -2,5 +2,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker stop vp0
 docker stop vp1
+docker stop ca
 docker rm vp0
 docker rm vp1
+docker rm ca
+

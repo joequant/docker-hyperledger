059ba27604439c98c52c8f588d1fa4e0ce6a58ec
Joseph C Wang <joequant@gmail.com>
2016-06-10 00:04:40 +0800

    fix restart if stopped

--- startnet.sh
+++ startnet.sh
@@ -10,7 +10,6 @@ docker rm ca
 docker run joequant/hyperledger echo
 
 docker run --name=vp0 \
-       --restart=unless-stopped \
        -v $SCRIPT_DIR:/local-dev \
        -p 5000:5000 \
        -p 30303:30303 \
@@ -20,15 +19,14 @@ docker run --name=vp0 \
        joequant/hyperledger peer node start --logging-level=debug >& vp0.log &
 
 docker run --name=vp1 \
-       --restart=unless-stopped \
        -v $SCRIPT_DIR:/local-dev \
        -i -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 -e \
 CORE_PEER_ID=vp1 -e CORE_PEER_ADDRESSAUTODETECT=true -e \
 CORE_PEER_DISCOVERY_ROOTNODE=172.17.0.2:30303 joequant/hyperledger \
 peer node start --logging-level=debug >& vp1.log &
 
+
 docker run --name=ca \
-       --restart=unless-stopped \
        -p 50051:50051 \
        -v $SCRIPT_DIR:/local-dev \
        -i -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 -e \

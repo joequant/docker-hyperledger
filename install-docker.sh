13c25e2281398e212fe56c5fdb00b9135954d649
Joseph C Wang <joequant@gmail.com>
2016-06-27 13:11:48 +0800

    fix install docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Please remember to unblock port 4243 from local firewall"
+echo "Please remember to unblock port 2375 from local firewall"
 cp docker-network.sysconfig /etc/sysconfig/docker-network
 systemctl restart docker

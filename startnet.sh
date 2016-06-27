51a87d3da261c51f2bfca0ba52405a80354e9f1b
Joseph C Wang <joequant@gmail.com>
2016-06-28 07:43:47 +0800

    change port

--- startnet.sh
+++ startnet.sh
@@ -15,24 +15,25 @@ docker run --name=vp0 \
        -p 5000:5000 \
        -p 30303:30303 \
        -e CORE_PEER_ID=vp0 \
-       -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 \
+       -e CORE_VM_ENDPOINT=http://172.17.0.1:2375 \
        -e CORE_PEER_ADDRESSAUTODETECT=true \
        joequant/hyperledger peer node start --logging-level=debug >& vp0.log &
 
 docker run --name=vp1 \
        -v $SCRIPT_DIR:/local-dev \
        -v $SCRIPT_DIR/git/fabric:/usr/lib/go-1.6/src/github.com/hyperledger/fabric \
-       -i -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 -e \
+       -i -e CORE_VM_ENDPOINT=http://172.17.0.1:2375 -e \
 CORE_PEER_ID=vp1 -e CORE_PEER_ADDRESSAUTODETECT=true -e \
 CORE_PEER_DISCOVERY_ROOTNODE=172.17.0.2:30303 joequant/hyperledger \
 peer node start --logging-level=debug >& vp1.log &
 
+sleep 15
 
 docker run --name=ca \
        -p 50051:50051 \
        -v $SCRIPT_DIR:/local-dev \
        -v $SCRIPT_DIR/git/fabric:/usr/lib/go-1.6/src/github.com/hyperledger/fabric \
-       -i -e CORE_VM_ENDPOINT=http://172.17.0.1:4243 -e \
+       -i -e CORE_VM_ENDPOINT=http://172.17.0.1:2375 -e \
 CORE_PEER_ID=ca -e CORE_PEER_ADDRESSAUTODETECT=true -e \
        CORE_PEER_DISCOVERY_ROOTNODE=172.17.0.2:30303 joequant/hyperledger \
        ./membersrvc --logging-level=debug >& ca.log &

eaefd4bd3944564bfd3c5eabab7a6fe8c954b33e
Joseph C Wang <joequant@gmail.com>
2016-07-08 02:36:58 +0800

    add host id for ca

--- startnet.sh
+++ startnet.sh
@@ -20,6 +20,8 @@ docker run --name=vp0 \
        -e CORE_PEER_ADDRESSAUTODETECT=true \
        joequant/hyperledger peer node start --logging-level=debug >& vp0.log &
 
+sleep 5
+
 docker run --name=vp1 \
        --hostname=vp1 \
        -v $SCRIPT_DIR:/local-dev \
@@ -29,13 +31,13 @@ docker run --name=vp1 \
        -e CORE_VM_ENDPOINT=http://172.17.0.1:2375 -e \
 CORE_PEER_ID=vp1 -e CORE_PEER_ADDRESSAUTODETECT=true -e \
 CORE_PEER_DISCOVERY_ROOTNODE=vp0:30303 joequant/hyperledger \
-peer node start --logging-level=debug >& vp1.log &
+       peer node start --logging-level=debug >& vp1.log &
 
-sleep 15
+sleep 5
 
 docker run --name=ca \
        -p 50051:50051 \
-       --hostname=vp1 \
+       --hostname=ca \
        --link vp0:vp0 \
        -v $SCRIPT_DIR:/local-dev \
        -v $SCRIPT_DIR/git/fabric:/usr/lib/go-1.6/src/github.com/hyperledger/fabric \
@@ -43,3 +45,9 @@ docker run --name=ca \
 CORE_PEER_ID=ca -e CORE_PEER_ADDRESSAUTODETECT=true -e \
        CORE_PEER_DISCOVERY_ROOTNODE=vp0:30303 joequant/hyperledger \
        ./membersrvc --logging-level=debug >& ca.log &
+
+sleep 5
+CA=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' ca`
+
+docker exec vp0 /bin/bash -c "echo $CA ca >> /etc/hosts"
+docker exec vp1 /bin/bash -c "echo $CA ca >> /etc/hosts"

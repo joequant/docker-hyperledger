bf441cbcb52123f50b43b089cb24bb1cce1e6a22
Joseph C Wang <joequant@gmail.com>
2016-07-08 02:08:49 +0800

    switch scripts to run test

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CORE_PEER_ADDRESS=172.17.0.2:30303
+export CORE_PEER_ADDRESS=vp0:30303
 
 NAME=`peer chaincode deploy -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'`
 

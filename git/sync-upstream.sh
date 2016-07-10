ea5932e93479f092b74dc21c30a3f32bb928b7b5
Joseph C Wang <joequant@gmail.com>
2016-07-10 20:51:56 +0800

    add car-lease-demo

--- git/sync-upstream.sh
+++ git/sync-upstream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in fabric hyperledger-fabric-js marbles chaincode-investigator cp-web cp-chaincode-v2 ; do
+for i in fabric hyperledger-fabric-js marbles chaincode-investigator cp-web cp-chaincode-v2 car-lease-demo; do
     pushd $i
     git fetch upstream
     git rebase upstream/master

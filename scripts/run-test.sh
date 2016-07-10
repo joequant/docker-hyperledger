4b43c3b9a51abb58e6423841d92f6eae60224659
Joseph C Wang <joequant@gmail.com>
2016-07-10 20:40:12 +0800

    add security

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export CORE_PEER_ADDRESS=vp0:30303
 
-NAME=`peer chaincode deploy -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'`
+NAME=`peer chaincode deploy -u jim -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'`
 
-peer chaincode invoke -n $NAME -c '{"Function": "invoke", "Args": ["a", "b", "10"]}'
-peer chaincode query -l golang -n $NAME -c '{"Function": "query", "Args": ["a"]}'
+peer chaincode invoke -n $NAME -u jim -c '{"Function": "invoke", "Args": ["a", "b", "10"]}'
+peer chaincode query -l golang -n $NAME -u jim -c '{"Function": "query", "Args": ["a"]}' 
+
--- startnet.sh
+++ startnet.sh
@@ -2,13 +2,26 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker stop vp0
 docker stop vp1
+docker stop ca
 docker rm vp0
 docker rm vp1
+docker rm ca
 
 docker run joequant/hyperledger echo
+docker run --name=ca \
+       -p 50051:50051 \
+       --hostname=ca \
+       -v $SCRIPT_DIR:/local-dev \
+       -v $SCRIPT_DIR/git/fabric:/usr/lib/go-1.6/src/github.com/hyperledger/fabric \
+       -i \
+       joequant/hyperledger \
+       ./membersrvc --logging-level=debug >& ca.log &
+
+sleep 5
 
 docker run --name=vp0 \
        --hostname=vp0 \
+       --link ca:ca \
        -v $SCRIPT_DIR:/local-dev \
        -v $SCRIPT_DIR/git/fabric:/usr/lib/go-1.6/src/github.com/hyperledger/fabric \
        -p 5000:5000 \
@@ -16,6 +29,13 @@ docker run --name=vp0 \
        -e CORE_PEER_ID=vp0 \
        -e CORE_VM_ENDPOINT=http://172.17.0.1:2375 \
        -e CORE_PEER_ADDRESSAUTODETECT=true \
+       -e CORE_SECURITY_ENABLED=true \
+       -e CORE_SECURITY_PRIVACY=true \
+       -e CORE_PEER_PKI_ECA_PADDR=ca:50051 \
+       -e CORE_PEER_PKI_TCA_PADDR=ca:50051 \
+       -e CORE_PEER_PKI_TLSCA_PADDR=ca:50051 \
+       -e CORE_SECURITY_ENROLLID=vp0 \
+       -e CORE_SECURITY_ENROLLSECRET=vp0_secret \
        joequant/hyperledger peer node start --logging-level=debug >& vp0.log &
 
 sleep 5
@@ -26,8 +46,19 @@ docker run --name=vp1 \
        -v $SCRIPT_DIR/git/fabric:/usr/lib/go-1.6/src/github.com/hyperledger/fabric \
        -i \
        --link vp0:vp0 \
+       --link ca:ca \
        -e CORE_VM_ENDPOINT=http://172.17.0.1:2375 -e \
-CORE_PEER_ID=vp1 -e CORE_PEER_ADDRESSAUTODETECT=true -e \
-CORE_PEER_DISCOVERY_ROOTNODE=vp0:30303 joequant/hyperledger \
+       CORE_PEER_ID=vp1 -e CORE_PEER_ADDRESSAUTODETECT=true \
+       -e CORE_PEER_DISCOVERY_ROOTNODE=vp0:30303 \
+       -e CORE_SECURITY_ENABLED=true \
+       -e CORE_SECURITY_PRIVACY=true \
+       -e CORE_PEER_PKI_ECA_PADDR=ca:50051 \
+       -e CORE_PEER_PKI_TCA_PADDR=ca:50051 \
+       -e CORE_PEER_PKI_TLSCA_PADDR=ca:50051 \
+       -e CORE_SECURITY_ENROLLID=vp1 \
+       -e CORE_SECURITY_ENROLLSECRET=vp1_secret \
+       joequant/hyperledger \
        peer node start --logging-level=debug >& vp1.log &
+
+
 
